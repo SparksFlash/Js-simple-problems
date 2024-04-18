6816ec229632b3c0bf0a6b974fbb1f6ffb77dc2f
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-04-19 00:19:51 +0600

    sum.js

---
+++ sum.js
@@ -0,0 +1,18 @@
+const numbers = [44, 23, 34, 32, 54, 5, 78];
+let sum = 0;
+for(let i = 0; i < numbers.length; i++){
+    sum += numbers[i];
+}
+//console.log(sum);
+
+function arrayTotal(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        sum = sum + element;
+    }
+    return sum;
+}
+
+const total = arrayTotal([30, 40, 60]);
+console.log('array total', total)
